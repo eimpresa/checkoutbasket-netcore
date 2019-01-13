e6af2a576ac14a23cb8161cf32978f04fe3d0148
Stefano Cocchi <stefano.cocchi.it@gmail.com>
2019-01-13 04:47:28 +0000

    Added get token regression test

---
+++ src/CheckoutBasket.Client.RegressionTests/AuthorizationTests.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace CheckoutBasket.Client.RegressionTests
+{
+    public class AuthorizationTests
+    {
+        private readonly TestSettings configuration;
+
+        public AuthorizationTests()
+        {
+            this.configuration = new TestSettings();
+        }
+
+        [Fact]
+        public async void ShouldGetTokenWithValidCredentials()
+        {
+            await GetTokenAsync(configuration.UserApiKey, configuration.UserApiSecret);
+        }
+
+        // [Fact]
+        // public async void ShouldNotGetTokenWithInvalidCredentials()
+        // {
+        //     var ex = await Assert.ThrowsAsync<InvalidOperationException>(async () => await GetTokenAsync(Guid.NewGuid().ToString(), Guid.NewGuid().ToString()));
+        // }        
+
+        private async Task<string> GetTokenAsync(string username, string password){
+            var postData = new Dictionary<string, string>() { { "username", configuration.UserApiKey }, { "password", configuration.UserApiSecret } };
+            string token;
+            using (var httpClient = new HttpClient())
+            {
+                var getTokenResponse = await httpClient.PostAsync(configuration.ApiBaseUrl + "authorize/token", new FormUrlEncodedContent(postData));
+                if (getTokenResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new InvalidOperationException("Cannot get security token: HTTP " + getTokenResponse.StatusCode);
+                }
+                var json = await getTokenResponse.Content.ReadAsStringAsync();
+                var tokenInfo = JsonConvert.DeserializeObject<AccessTokenDto>(json);
+                token = tokenInfo.token;
+            }
+
+            Assert.NotNull(token);
+            return token;
+        }
+
+        private class AccessTokenDto
+        {
+            public string token { get; set; }
+            public int expires_int { get; set; }
+        }
+    }
+}
---
+++ src/CheckoutBasket.Client.RegressionTests/TestSettings.cs
@@ -0,0 +1,12 @@
+using System.Net;
+
+namespace CheckoutBasket.Client.RegressionTests
+{
+    public class TestSettings
+    {
+        public string ApiBaseUrl => "http://localhost:55687/api/v1/";
+        public string UserApiKey => "bqEEg3c2HLM4jMXLvxDBboQzYGn2JuNPoL7tDSV4";
+        public string UserApiSecret => "vQwJgfbNGWmEj5WMxy3ZPCJKZxTYvBCCTJzekBd3";
+
+    }
+}
